a2c5d908c74c68318c027ac2bacd872610f3a76c
Chelovek12346 <sdanik228228@gmail.com>
2026-04-26 10:03:28 +0500

    stuped commit

--- funding/urls.py
+++ funding/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 app_name = "funding"
 
-
+urlpatterns = [
+    path('summary/', views.funding_summary),
+    path('donate/', views.donate),
+]
 
--- funding/views.py
+++ funding/views.py
@@ -1,5 +1,35 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from django.shortcuts import render
-from .service import *
 
-# Create your views here.
+from .models import StudentProfile, Donation
+from .service import FundingService
 
+
+@api_view(['GET'])
+def funding_summary(request):
+    student = request.user.studentprofile
+    data = FundingService.get_summary(student)
+    return Response(data)
+
+
+@api_view(['POST'])
+def donate(request):
+    student_id = request.data.get("student_id")
+    amount = float(request.data.get("amount"))
+
+    student = StudentProfile.objects.get(id=student_id)
+
+    Donation.objects.create(
+        student=student,
+        amount=amount
+    )
+
+    return Response({"status": "ok"})
+
+
+def funding_summary(request):
+    return render(request, 'funding/fundraising.html')
+
+def donate(request):
+    return render(request, 'funding/donation.html')
